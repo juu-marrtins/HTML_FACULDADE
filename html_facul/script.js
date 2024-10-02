62b1fb3157fe076a5b2d4bd68377224340159212
Julia Martins <juliagabrielly.m.araujo@gmail.com>
2024-10-02 19:25:09 -0300

    Feat: Declaração de Variaveis

---
+++ html_facul/script.js
@@ -0,0 +1,33 @@
+// Declarações de variaveis
+var time = "Palmeiras";
+let idade = 25;
+idade = "25 anos";
+const cidade = "São Paulo";
+
+//Primitivos 
+let saudacao = "Olá Mundo";
+let preco = 19.99
+let estaChovendo = false;
+
+// Complexo
+let teclado = {
+    marca: "Hp",
+    peso: "1kg",
+    cor: "Branco",
+    tipo: "Mecanico"
+}
+
+
+teclado.marca="Dell"; //Para modificar valores do objeto
+console.log(teclado)
+
+let cores = ["Vermelho", "Azul", "Laranja"];
+
+// Concatenção entre strings
+
+let nome = "Matheus";
+let message = "Olá " + nome + ", seja bem-vindo.";
+console.log(message);
+
+let str = "123";
+let strConvertido = Number(str);
